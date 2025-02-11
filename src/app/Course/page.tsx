0ce347c9d9a490fc188b08f1ec7f916a0b912f27
Pone <insysiengmaiphouthasone@gmail.com>
2025-02-11 08:58:12 +0700

    6 commit

--- src/app/Course/page.tsx
+++ src/app/Course/page.tsx
@@ -2,46 +2,53 @@ import Image from "next/image";
 
 export default function Course() {
   return (
-    <>
-      {/* ตารางที่ 1 */}
-      <table className="border-collapse border border-red-500 w-full text-center">
+    <div className="flex justify-center  mt-4">
+      
+      <table className="border-collapse border border-red-500 w-1/2 text-center">
         <thead>
           <tr className="bg-red-500 text-white">
             <th className="border border-white p-2">ຫລັກສູດ</th>
-            <th className="border border-white p-2">1</th>
-            <th className="border border-white p-2">2</th>
-            <th className="border border-white p-2">3</th>
+            <td className="border border-red-500 p-2">Data</td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-red-500 p-2">Data</td>
-            <td className="border border-red-500 p-2">A</td>
-            <td className="border border-red-500 p-2">B</td>
-            <td className="border border-red-500 p-2">C</td>
+            <td className="border border-red-500 p-2">1</td>
+            <td className="border border-red-500 p-2">ITBM</td>
+          </tr>
+          <tr>
+            <td className="border border-red-500 p-2">2</td>
+            <td className="border border-red-500 p-2">ITBM</td>
+          </tr>
+          <tr>
+            <td className="border border-red-500 p-2">3</td>
+            <td className="border border-red-500 p-2">ITBM</td>
           </tr>
         </tbody>
       </table>
 
-      {/* ตารางที่ 2 */}
-      <table className="border-collapse border border-gray-500 w-full text-center mt-4">
+      <table className="border-collapse border border-red-500 w-1/2 text-center">
         <thead>
-          <tr className="bg-gray-500 text-white">
+          <tr className="bg-red-500 text-white">
             <th className="border border-white p-2">ຫລັກສູດ</th>
-            <th className="border border-white p-2">Applicant</th>
-            <th className="border border-white p-2">2</th>
-            <th className="border border-white p-2">3</th>
+            <td className="border border-red-500 p-2">Data</td>
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td className="border border-gray-500 p-2">Data</td>
-            <td className="border border-gray-500 p-2">X</td>
-            <td className="border border-gray-500 p-2">Y</td>
-            <td className="border border-gray-500 p-2">Z</td>
+            <td className="border border-red-500 p-2">1</td>
+            <td className="border border-red-500 p-2">ITBM</td>
+          </tr>
+          <tr>
+            <td className="border border-red-500 p-2">2</td>
+            <td className="border border-red-500 p-2">ITBM</td>
+          </tr>
+          <tr>
+            <td className="border border-red-500 p-2">3</td>
+            <td className="border border-red-500 p-2">ITBM</td>
           </tr>
         </tbody>
       </table>
-    </>
+    </div>
   );
 }
